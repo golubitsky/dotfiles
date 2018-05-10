0450b8c58458ead3db4b22c1df7e2a47bd3c74d0
Mike Golubitsky <mike617@gmail.com>
2018-05-10 18:35:39 -0400

    first

---
+++ conda_auto_env.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# source: https://github.com/chdoig/conda-auto-env
+
+# conda-auto-env automatically activates a conda environment when
+# entering a folder with an environment.yml file.
+#
+# If the environment doesn't exist, conda-auto-env creates it and
+# activates it for you.
+#
+# To install add this line to your .bashrc or .bash-profile:
+#
+#       source /path/to/conda_auto_env.sh
+#
+
+function conda_auto_env() {
+  if [ -e "environment.yml" ]; then
+    # echo "environment.yml file found"
+    ENV=$(head -n 1 environment.yml | cut -f2 -d ' ')
+    # Check if you are already in the environment
+    if [[ $PATH != *$ENV* ]]; then
+      # Check if the environment exists
+      source activate $ENV
+      if [ $? -eq 0 ]; then
+        echo "Activating conda env '$ENV'"
+        conda env list | grep '*'
+      else
+        # Create the environment and activate
+        echo "Conda env '$ENV' doesn't exist."
+        conda env create -q
+        source activate $ENV
+        echo "Created and activated conda env '$ENV'"
+        conda env list | grep '*'
+      fi
+    fi
+  else
+    # Ensure we are in base environment
+    source activate base
+  fi
+}
+
+export PROMPT_COMMAND=conda_auto_env
---
+++ readme.md
@@ -0,0 +1,7 @@
+# conda_auto_env.sh
+
+Auto switching of conda envs, inspired by [this](https://github.com/chdoig/conda-auto-env) with a couple of modifications:
+* notify of having switched to environment
+* ensure `base` env selected when `environment.yml` not present
+
+Note that this requires the `conda` command.
